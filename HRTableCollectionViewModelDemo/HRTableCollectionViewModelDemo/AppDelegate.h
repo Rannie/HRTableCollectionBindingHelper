c8ed40434e82288e603f596d7df95c2cfd81905f
Rannie <18622218653@163.com>
2014-11-04 23:51:17 +0800

    init

---
+++ HRTableCollectionViewModelDemo/HRTableCollectionViewModelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HRTableCollectionViewModelDemo
+//
+//  Created by Rannie on 14/11/4.
+//  Copyright (c) 2014年 Rannie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HRTableCollectionViewModelDemo/HRTableCollectionViewModelDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HRTableCollectionViewModelDemo
+//
+//  Created by Rannie on 14/11/4.
+//  Copyright (c) 2014年 Rannie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
