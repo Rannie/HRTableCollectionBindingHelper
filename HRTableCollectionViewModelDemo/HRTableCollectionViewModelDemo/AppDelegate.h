1db17197190af69adba642f4f02774f470a11f88
Hanran Liu <cocoarannie@163.com>
2014-11-05 11:24:23 +0800

    init

---
+++ HRTableCollectionViewModelDemo/HRTableCollectionViewModelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HRTableCollectionViewModelDemo
+//
+//  Created by Ran on 14/11/5.
+//  Copyright (c) 2014年 Rannie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
